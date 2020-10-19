2b263b370d6e4ea1fa43b02247d3692fba766114
Tor-Christian Tangenes <40393162+torchr89@users.noreply.github.com>
2020-10-19 13:06:42 +0200

    Vault (#2)
    * Enable Vault
    * Read from file

--- src/main/kotlin/no/nav/syfo/Environment.kt
+++ src/main/kotlin/no/nav/syfo/Environment.kt
@@ -1,9 +1,22 @@
 package no.nav.syfo
 
+import java.nio.file.Files
+import java.nio.file.Paths
+
 data class Environment(
         val applicationPort: Int = getEnvVar("APPLICATION_PORT", "8080").toInt(),
-        val applicationThreads: Int = getEnvVar("APPLICATION_THREADS", "4").toInt()
+        val applicationThreads: Int = getEnvVar("APPLICATION_THREADS", "4").toInt(),
+        val serviceUserSecrets: ServiceUserSecrets = ServiceUserSecrets()
 )
+
+data class ServiceUserSecrets (
+    val username: String = readFileFromPath("/var/run/secrets/serviceuser/username"),
+    val password: String = readFileFromPath("/var/run/secrets/serviceuser/password")
+)
+
+fun readFileFromPath(path: String) : String {
+    return Files.readString(Paths.get(path))
+}
 
 fun getEnvVar(varName: String, defaultValue: String? = null) =
         System.getenv(varName) ?: defaultValue ?: throw RuntimeException("Missing required variable \"$varName\"")
