daf25c79f74977fb9e7002cd61686fe0dd7604bf
Tor-Christian Tangenes <40393162+torchr89@users.noreply.github.com>
2021-04-22 09:57:30 +0200

    Remove syfoservice-based logic for calculating 'Varsel' since we don't have direct access to 'Sykmelding'-data. Will use syfosyketilfelle services instead (#13)

--- src/main/kotlin/no/nav/syfo/domain/AktoerIdConsumer.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.syfo.domain
-
-class AktoerIdConsumer {
-    val aktoerId: String = ""
-
-    fun finnAktoerId(id: String): String {
-        return aktoerId
-    }
-}
--- src/main/kotlin/no/nav/syfo/domain/HendelseType.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.syfo.domain
-
-enum class HendelseType {
-    AKTIVITETSKRAV_VARSEL,
-    SVAR_MOTEBEHOV,
-    MER_VEILEDNING,
-    DEFAULT,
-}
