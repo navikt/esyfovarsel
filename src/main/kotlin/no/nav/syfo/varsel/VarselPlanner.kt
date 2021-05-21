d7b158845bfe5cf4157b25c4284be4e0382f68b1
Tor-Christian Tangenes <torchrct@gmail.com>
2021-05-21 09:51:31 +0200

    Change endpoint to fetch more data

--- src/main/kotlin/no/nav/syfo/varsel/VarselPlanner.kt
+++ src/main/kotlin/no/nav/syfo/varsel/VarselPlanner.kt
@@ -1,7 +1,7 @@
 package no.nav.syfo.varsel
 
-import no.nav.syfo.consumer.domain.Oppfolgingstilfelle
+import no.nav.syfo.consumer.domain.OppfolgingstilfellePerson
 
 interface VarselPlanner {
-    fun processOppfolgingstilfelle(oppfolgingstilfelle: Oppfolgingstilfelle)
+    fun processOppfolgingstilfelle(oppfolgingstilfellePerson: OppfolgingstilfellePerson)
 }
