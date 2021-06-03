ba1fc235c414af6b81f535b0ba60d1cc0faebc8b
Tanja Krasnomovets <tanja.krasnomovets@nav.no>
2021-06-03 16:12:15 +0200

    ZsJ3znKz: FFlyttet DTOer fra domain dir

--- src/main/kotlin/no/nav/syfo/consumer/domain/SykmeldingStatus.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.syfo.consumer.domain
-
-import java.time.OffsetDateTime
-
-data class SykmeldingStatus (
-    var statusEvent: String? = null,
-    var timestamp: OffsetDateTime? = null,
-    var arbeidsgiver: ArbeidsgiverStatus? = null
-)
