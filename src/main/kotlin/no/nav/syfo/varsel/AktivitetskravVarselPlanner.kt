00cf89a1a851f40444692e271cbc8933a2de4c7b
Tanja Krasnomovets <tanja.krasnomovets@nav.no>
2021-06-11 13:37:34 +0200

    ZsJ3znKz: Kommenterte sjekk for gradert SM inntil ny AAD API til sufosmregister er pa plass.

--- src/main/kotlin/no/nav/syfo/varsel/AktivitetskravVarselPlanner.kt
+++ src/main/kotlin/no/nav/syfo/varsel/AktivitetskravVarselPlanner.kt
@@ -71,10 +71,11 @@ class AktivitetskravVarselPlanner(val databaseAccess: DatabaseInterface, val syk
                     varselUtil.isVarselDatoEtterTilfelleSlutt(aktivitetskravVarselDato, forlopSluttDato) -> {
                         log.info("[AKTIVITETSKRAV_VARSEL]: Tilfelle er kortere enn 6 uker")
                     }
-                    sykmeldingService.isNot100SykmeldtPaVarlingsdato(aktivitetskravVarselDato, arbeidstakerFnr!!) -> {
-                        log.info("[AKTIVITETSKRAV_VARSEL]: Sykmeldingsgrad er < enn 100% på beregnet varslingsdato")
-                    }
-                    varselUtil.isVarselPlanlagt(arbeidstakerFnr, VarselType.AKTIVITETSKRAV, aktivitetskravVarselDato) -> {
+                    //TODO: uncomment when new AAD is implemented in team-sykmelding
+//                    sykmeldingService.isNot100SykmeldtPaVarlingsdato(aktivitetskravVarselDato, arbeidstakerFnr!!) -> {
+//                        log.info("[AKTIVITETSKRAV_VARSEL]: Sykmeldingsgrad er < enn 100% på beregnet varslingsdato")
+//                    }
+                    varselUtil.isVarselPlanlagt(arbeidstakerFnr!!, VarselType.AKTIVITETSKRAV, aktivitetskravVarselDato) -> {
                         log.info("[AKTIVITETSKRAV_VARSEL]: varsel er allerede planlagt")
                     }
                     varselUtil.isVarselSendUt(arbeidstakerFnr, VarselType.AKTIVITETSKRAV, aktivitetskravVarselDato) -> {
