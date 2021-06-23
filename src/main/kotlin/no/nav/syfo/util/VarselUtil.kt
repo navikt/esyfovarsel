4ed949e8f6006bf8fa7c9b304d8afb92487ac9bf
Tanja Krasnomovets <tanja.krasnomovets@nav.no>
2021-06-23 15:25:03 +0200

    ZsJ3znKz: Lagt til logging

--- src/main/kotlin/no/nav/syfo/util/VarselUtil.kt
+++ src/main/kotlin/no/nav/syfo/util/VarselUtil.kt
@@ -3,9 +3,13 @@ package no.nav.syfo.util
 import no.nav.syfo.db.DatabaseInterface
 import no.nav.syfo.db.domain.VarselType
 import no.nav.syfo.db.fetchPlanlagtVarselByFnr
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import java.time.LocalDate
 
 class VarselUtil(private val databaseAccess: DatabaseInterface) {
+    private val log: Logger = LoggerFactory.getLogger("no.nav.syfo.varsel.VarselUtil")
+
     fun isVarselDatoForIDag(varselDato: LocalDate): Boolean {
         return varselDato.isBefore(LocalDate.now())
     }
@@ -15,8 +19,11 @@ class VarselUtil(private val databaseAccess: DatabaseInterface) {
     }
 
     fun isVarselPlanlagt(fnr: String, varselType: VarselType, varselDato: LocalDate): Boolean {
-        return databaseAccess.fetchPlanlagtVarselByFnr(fnr)
-            .filter { varselType.name == it.type }
+        val varsler = databaseAccess.fetchPlanlagtVarselByFnr(fnr)
+
+        log.info("[AKTIVITETSKRAV_VARSEL]: tidligere lagrete varsler:  $varsler")//TODO: delete
+
+        return varsler.filter { varselType.name == it.type }
             .filter { it.utsendingsdato === varselDato }
             .any()
     }
