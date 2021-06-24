af21917d4192da99a5c1cf7b0c268ca3dc30f10b
Tanja Krasnomovets <tanja.krasnomovets@nav.no>
2021-06-24 22:34:49 +0200

    ZsJ3znKz: WIP: Lager sykerforlop

--- src/main/kotlin/no/nav/syfo/consumer/domain/Sykeforlop.kt
+++ src/main/kotlin/no/nav/syfo/consumer/domain/Sykeforlop.kt
@@ -3,7 +3,7 @@ package no.nav.syfo.consumer.domain
 import java.time.LocalDate
 
 data class Sykeforlop(
-    val ressursIds: MutableList<String>,
-    val fom: LocalDate,
+    val ressursIds: MutableSet<String>,
+    var fom: LocalDate,
     var tom: LocalDate
 )
