a8f56c6e65a8a110289c99949644a809d6603b84
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2021-11-24 10:14:43 +0100

    Skriv ut antall varsler som er replanlagt

--- src/main/kotlin/no/nav/syfo/api/admin/AdminRest.kt
+++ src/main/kotlin/no/nav/syfo/api/admin/AdminRest.kt
@@ -1,11 +1,10 @@
 package no.nav.syfo.api.admin
 
 import io.ktor.application.*
-import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.util.pipeline.*
-import no.nav.syfo.consumer.DkifConsumer
+import no.nav.syfo.db.domain.VarselType
 import no.nav.syfo.service.ReplanleggingService
 import java.time.LocalDate
 
@@ -16,14 +15,14 @@ fun Route.registerAdminApi(
     val toParam = "to"
     get("/admin/replanleggMerVeiledningVarsler") {
         // from og to parametere må være på formatet "2007-12-03"
-        replanleggingService.planleggMerVeiledningVarslerPaNytt(parseDate(fromParam), parseDate(toParam))
-        call.respond(HttpStatusCode.OK)
+        val antallVarsler = replanleggingService.planleggMerVeiledningVarslerPaNytt(parseDate(fromParam), parseDate(toParam))
+        call.respondText("Planla $antallVarsler av typen ${VarselType.MER_VEILEDNING}")
     }
 
     get("/admin/replanleggAktivitetskravVarsler") {
         // from og to parametere må være på formatet "2007-12-03"
-        replanleggingService.planleggAktivitetskravVarslerPaNytt(parseDate(fromParam), parseDate(toParam))
-        call.respond(HttpStatusCode.OK)
+        val antallVarsler = replanleggingService.planleggAktivitetskravVarslerPaNytt(parseDate(fromParam), parseDate(toParam))
+        call.respondText("Planla $antallVarsler av typen ${VarselType.AKTIVITETSKRAV}")
     }
 }
 
--- src/main/kotlin/no/nav/syfo/service/ReplanleggingService.kt
+++ src/main/kotlin/no/nav/syfo/service/ReplanleggingService.kt
@@ -10,22 +10,28 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.time.LocalDate
 
-class ReplanleggingService(val databaseAccess: DatabaseInterface, val merVeiledningVarselPlanner: MerVeiledningVarselPlanner, val aktivitetskravVarselPlanner: AktivitetskravVarselPlanner) {
+class ReplanleggingService(
+    val databaseAccess: DatabaseInterface,
+    val merVeiledningVarselPlanner: MerVeiledningVarselPlanner,
+    val aktivitetskravVarselPlanner: AktivitetskravVarselPlanner
+) {
 
     private val log: Logger = LoggerFactory.getLogger("no.nav.syfo.service.ReplanleggingService")
 
-    suspend fun planleggMerVeiledningVarslerPaNytt(fromDate: LocalDate, toDate: LocalDate) {
-        planleggVarslerPaNytt(VarselType.MER_VEILEDNING, merVeiledningVarselPlanner, fromDate, toDate)
+    suspend fun planleggMerVeiledningVarslerPaNytt(fromDate: LocalDate, toDate: LocalDate): Int {
+        return planleggVarslerPaNytt(VarselType.MER_VEILEDNING, merVeiledningVarselPlanner, fromDate, toDate)
     }
 
-    suspend fun planleggAktivitetskravVarslerPaNytt(fromDate: LocalDate, toDate: LocalDate) {
-        planleggVarslerPaNytt(VarselType.AKTIVITETSKRAV, aktivitetskravVarselPlanner, fromDate, toDate)
+    suspend fun planleggAktivitetskravVarslerPaNytt(fromDate: LocalDate, toDate: LocalDate): Int {
+        return planleggVarslerPaNytt(VarselType.AKTIVITETSKRAV, aktivitetskravVarselPlanner, fromDate, toDate)
     }
 
-    suspend fun planleggVarslerPaNytt(varselType: VarselType, planlegger: VarselPlanner, fromDate: LocalDate, toDate: LocalDate) {
+    suspend fun planleggVarslerPaNytt(varselType: VarselType, planlegger: VarselPlanner, fromDate: LocalDate, toDate: LocalDate): Int {
         log.info("[ReplanleggingService]: Går gjennom alle planlagte $varselType-varsler mellom $fromDate og $toDate og planlegger dem på nytt")
         val planlagteVarsler = databaseAccess.fetchPlanlagtVarselByTypeAndUtsendingsdato(varselType, fromDate, toDate)
-        log.info("[ReplanleggingService]: Replanlegger ${planlagteVarsler.size} varsler av type $varselType")
-        planlagteVarsler.forEach{it -> planlegger.processOppfolgingstilfelle(it.aktorId, it.fnr)}
+        val size = planlagteVarsler.size
+        log.info("[ReplanleggingService]: Replanlegger $size varsler av type $varselType")
+        planlagteVarsler.forEach { planlegger.processOppfolgingstilfelle(it.aktorId, it.fnr) }
+        return size
     }
 }
