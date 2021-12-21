60f1af7bc7582eb1d7531172179b0831581440d7
Tanja Krasnomovets <Vitenok@users.noreply.github.com>
2021-12-21 14:06:11 +0100

    Add sorting to gyldigeSykmeldingTilfelledager list; Rename isBrukerGradert in PdlConsumer as it collides with gradert sykmelding term (#80)

--- src/main/kotlin/no/nav/syfo/service/AccessControl.kt
+++ src/main/kotlin/no/nav/syfo/service/AccessControl.kt
@@ -6,7 +6,7 @@ import no.nav.syfo.consumer.PdlConsumer
 class AccessControl(val pdlConsumer: PdlConsumer, val dkifConsumer: DkifConsumer) {
 
     fun getFnrIfUserCanBeNotified(aktorId: String): String? {
-        return if (pdlConsumer.isBrukerGradert(aktorId) == false && dkifConsumer.kontaktinfo(aktorId)?.kanVarsles == true)
+        return if (pdlConsumer.isBrukerGradertForInformasjon(aktorId) == false && dkifConsumer.kontaktinfo(aktorId)?.kanVarsles == true)
             return pdlConsumer.getFnr(aktorId)
         else
             null
