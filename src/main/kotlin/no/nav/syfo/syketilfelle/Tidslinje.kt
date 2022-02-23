a5869147cdbfc41297e9317fceb90d7a1b8b6c82
Tor-Christian Tangenes <torchrct@gmail.com>
2022-02-23 11:59:33 +0100

    Catch riktig exception-type

--- src/main/kotlin/no/nav/syfo/syketilfelle/Tidslinje.kt
+++ src/main/kotlin/no/nav/syfo/syketilfelle/Tidslinje.kt
@@ -2,6 +2,7 @@ package no.nav.syfo.syketilfelle.domain
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.lang.IllegalArgumentException
 import java.time.temporal.ChronoUnit
 
 class Tidslinje(private val syketilfellebiter: Syketilfellebiter) {
@@ -21,7 +22,7 @@ class Tidslinje(private val syketilfellebiter: Syketilfellebiter) {
         // TODO: Fjern try-catch når flex-syketilfellebiter topic er i sync med oppfølgingstilfeller
         try {
             require(syketilfellebiter.biter.isNotEmpty())
-        } catch (e: IllegalStateException) {
+        } catch (e: IllegalArgumentException) {
             log.warn("Spør på fnr som ikke har noen syketilfellebiter i databasen")
         }
 
