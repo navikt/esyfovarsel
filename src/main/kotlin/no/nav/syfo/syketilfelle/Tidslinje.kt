662ee03f5791de492ba1fc7323f4daa11dd22eeb
Tor-Christian Tangenes <40393162+torchr89@users.noreply.github.com>
2022-02-23 13:01:40 +0100

    Fjern støyende logging, og catch/logg ut generell exception. Logg manglende syketilfellebiter for Warning (#96)
    * Logg manglende syketilfellebiter som Warning
    Fjern støyende logging, og catch/logg ut generell exception

--- src/main/kotlin/no/nav/syfo/syketilfelle/Tidslinje.kt
+++ src/main/kotlin/no/nav/syfo/syketilfelle/Tidslinje.kt
@@ -1,10 +1,14 @@
 package no.nav.syfo.syketilfelle.domain
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.lang.IllegalArgumentException
 import java.time.temporal.ChronoUnit
 
 class Tidslinje(private val syketilfellebiter: Syketilfellebiter) {
 
     private val tidslinjeliste: List<Syketilfelledag>
+    private val log: Logger = LoggerFactory.getLogger("no.nav.syfo.syketilfelle.domain.Tidslinje")
 
     init {
         tidslinjeliste = genererTidslinje()
@@ -15,7 +19,12 @@ class Tidslinje(private val syketilfellebiter: Syketilfellebiter) {
     }
 
     private fun genererTidslinje(): List<Syketilfelledag> {
-        require(syketilfellebiter.biter.isNotEmpty())
+        // TODO: Fjern try-catch når flex-syketilfellebiter topic er i sync med oppfølgingstilfeller
+        try {
+            require(syketilfellebiter.biter.isNotEmpty())
+        } catch (e: IllegalArgumentException) {
+            log.warn("Spør på fnr som ikke har noen syketilfellebiter i databasen")
+        }
 
         val tidligsteFom = syketilfellebiter.finnTidligsteFom()
         val sensesteTom = syketilfellebiter.finnSenesteTom()
