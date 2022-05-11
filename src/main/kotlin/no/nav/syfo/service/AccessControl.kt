de8dfa18934cd29a0c53724400bbd53d74618dda
Tanja Krasnomovets <tanja.krasnomovets@nav.no>
2022-05-11 14:38:36 +0200

    Add log

--- src/main/kotlin/no/nav/syfo/service/AccessControl.kt
+++ src/main/kotlin/no/nav/syfo/service/AccessControl.kt
@@ -1,7 +1,7 @@
 package no.nav.syfo.service
 
-import no.nav.syfo.consumer.dkif.DkifConsumer
 import no.nav.syfo.consumer.PdlConsumer
+import no.nav.syfo.consumer.dkif.DkifConsumer
 import org.slf4j.LoggerFactory
 
 class AccessControl(val pdlConsumer: PdlConsumer, val dkifConsumer: DkifConsumer) {
@@ -9,6 +9,8 @@ class AccessControl(val pdlConsumer: PdlConsumer, val dkifConsumer: DkifConsumer
     val log = LoggerFactory.getLogger("no.nav.syfo.service.AccessControl")
 
     fun getFnrIfUserCanBeNotified(aktorId: String): String? {
+        log.info("pdlConsumer.isBrukerGradertForInformasjon(aktorId): ${pdlConsumer.isBrukerGradertForInformasjon(aktorId)}") // TODO Delete
+        log.info("dkifConsumer.kontaktinfo(aktorId)?.kanVarsles: ${dkifConsumer.kontaktinfo(aktorId)?.kanVarsles}") // TODO Delete
         return if (pdlConsumer.isBrukerGradertForInformasjon(aktorId) == false && dkifConsumer.kontaktinfo(aktorId)?.kanVarsles == true)
             pdlConsumer.getFnr(aktorId)
         else
