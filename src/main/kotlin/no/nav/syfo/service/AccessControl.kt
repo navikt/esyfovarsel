d2f5595d85c2f74639fbb860c2707329530b9f89
Tanja Krasnomovets <tanja.krasnomovets@nav.no>
2022-05-12 12:25:49 +0200

    wip: Remove redundant logging

--- src/main/kotlin/no/nav/syfo/service/AccessControl.kt
+++ src/main/kotlin/no/nav/syfo/service/AccessControl.kt
@@ -9,8 +9,6 @@ class AccessControl(val pdlConsumer: PdlConsumer, val dkifConsumer: DkifConsumer
     val log = LoggerFactory.getLogger("no.nav.syfo.service.AccessControl")
 
     fun getFnrIfUserCanBeNotified(aktorId: String): String? {
-        log.info("pdlConsumer.isBrukerGradertForInformasjon(aktorId): ${pdlConsumer.isBrukerGradertForInformasjon(aktorId)}") // TODO Delete
-        log.info("dkifConsumer.kontaktinfo(aktorId)?.kanVarsles: ${dkifConsumer.kontaktinfo(aktorId)?.kanVarsles}") // TODO Delete
         return if (pdlConsumer.isBrukerGradertForInformasjon(aktorId) == false && dkifConsumer.kontaktinfo(aktorId)?.kanVarsles == true)
             pdlConsumer.getFnr(aktorId)
         else
