a97d38af55f774af8177d964f0e0a67d65dd2254
Kaja Kvinge Akselsen <75254007+kaja-nav@users.noreply.github.com>
2022-05-12 10:28:23 +0200

    Add support for svar møtebehov (#113)

---
+++ src/main/kotlin/no/nav/syfo/kafka/varselbus/domain/MotebehovNLVarselData.kt
@@ -0,0 +1,8 @@
+package no.nav.syfo.kafka.varselbus.domain
+
+import java.io.Serializable
+
+data class MotebehovNLVarselData (
+    val ansattFnr: String,
+    val orgnummer: String
+) : Serializable
