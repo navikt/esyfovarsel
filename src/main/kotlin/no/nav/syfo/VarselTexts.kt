32a29a67c702bbb99504425a2754cd95681def33
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2022-05-23 08:41:58 +0200

    Update text

--- src/main/kotlin/no/nav/syfo/VarselTexts.kt
+++ src/main/kotlin/no/nav/syfo/VarselTexts.kt
@@ -4,4 +4,4 @@ const val DINE_SYKMELDTE_OPPFOLGINGSPLAN_OPPRETTET_TEKST = "1 oppfølgingsplan e
 const val DINE_SYKMELDTE_OPPFOLGINGSPLAN_SENDT_TIL_GODKJENNING_TEKST = "1 oppfølgingsplan til godkjenning"
 const val DINE_SYKMELDTE_DIALOGMOTE_SVAR_MOTEBEHOV_TEKST = "1 ny forespørsel om behov for dialogmøte"
 
-const val BRUKERNOTIFIKASJONER_DIALOGMOTE_SVAR_MOTEBEHOV_TEKST = "Du har fått 1 ny forespørsel om behov for dialogmøte"
+const val BRUKERNOTIFIKASJONER_DIALOGMOTE_SVAR_MOTEBEHOV_TEKST = "NAV ønsker svar på om du trenger et dialogmøte"
