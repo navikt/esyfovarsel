cd96f39c5520f916a8a6f79d959ce26ef58509e9
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2022-06-09 11:18:10 +0200

    Send varsler from 2022-06-07, since the job failed that day

--- src/main/kotlin/no/nav/syfo/job/VarselSender.kt
+++ src/main/kotlin/no/nav/syfo/job/VarselSender.kt
@@ -33,7 +33,7 @@ class VarselSender(
 
         log.info("Starter SendVarslerJobb")
 
-        val varslerToSendToday = databaseAccess.fetchPlanlagtVarselByUtsendingsdato(LocalDate.parse("2022-06-06"))
+        val varslerToSendToday = databaseAccess.fetchPlanlagtVarselByUtsendingsdato(LocalDate.parse("2022-06-07"))
         log.info("Planlegger å sende ${varslerToSendToday.size} varsler")
 
         if (!toggles.sendAktivitetskravVarsler) log.info("Utsending av Aktivitetskrav er ikke aktivert, og varsler av denne typen blir ikke sendt")
