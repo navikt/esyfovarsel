c4496bbd77dd3e571cdb9d4b217a89923b23cf15
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2022-06-17 14:07:27 +0200

    Fix typo in error message

--- src/main/kotlin/no/nav/syfo/consumer/syfomotebehov/SyfoMotebehovConsumer.kt
+++ src/main/kotlin/no/nav/syfo/consumer/syfomotebehov/SyfoMotebehovConsumer.kt
@@ -51,7 +51,7 @@ open class SyfoMotebehovConsumer(urlEnv: UrlEnv, private val tokenConsumer: Toke
                 body = requestBody
             }
             if (response.status != HttpStatusCode.OK) {
-                throw RuntimeException("Klarte ikke å opprette varsel til naermeste leder om møtebehov - ${response.status} $requestURL")
+                throw RuntimeException("Klarte ikke å opprette varsel til sykmeldt om møtebehov - ${response.status} $requestURL")
             }
         }
     }
