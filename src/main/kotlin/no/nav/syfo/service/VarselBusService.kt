490ae1ecb4304f1fd9269621fe2390ae28ef80c1
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2022-08-17 12:38:49 +0200

    Refactor EsyfovarselHendelse

--- src/main/kotlin/no/nav/syfo/service/VarselBusService.kt
+++ src/main/kotlin/no/nav/syfo/service/VarselBusService.kt
@@ -1,11 +1,10 @@
 package no.nav.syfo.service
 
-import no.nav.syfo.kafka.producers.dinesykmeldte.DineSykmeldteHendelseKafkaProducer
+import no.nav.syfo.kafka.consumers.varselbus.domain.ArbeidstakerHendelse
 import no.nav.syfo.kafka.consumers.varselbus.domain.EsyfovarselHendelse
-import no.nav.syfo.kafka.consumers.varselbus.domain.HendelseType.NL_DIALOGMOTE_SVAR_MOTEBEHOV
-import no.nav.syfo.kafka.consumers.varselbus.domain.HendelseType.NL_OPPFOLGINGSPLAN_OPPRETTET
-import no.nav.syfo.kafka.consumers.varselbus.domain.HendelseType.NL_OPPFOLGINGSPLAN_SENDT_TIL_GODKJENNING
-import no.nav.syfo.kafka.consumers.varselbus.domain.HendelseType.SM_DIALOGMOTE_SVAR_MOTEBEHOV
+import no.nav.syfo.kafka.consumers.varselbus.domain.HendelseType.*
+import no.nav.syfo.kafka.consumers.varselbus.domain.NarmesteLederHendelse
+import no.nav.syfo.kafka.producers.dinesykmeldte.DineSykmeldteHendelseKafkaProducer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -15,13 +14,25 @@ class VarselBusService(
 ) {
     private val log: Logger = LoggerFactory.getLogger("no.nav.syfo.service.VarselBusService")
     private val oppfolgingsplanVarselService = OppfolgingsplanVarselService(dineSykmeldteHendelseKafkaProducer)
-    suspend fun processVarselHendelse(varselHendelse: EsyfovarselHendelse) {
+    fun processVarselHendelse(varselHendelse: EsyfovarselHendelse) {
         log.info("Behandler varsel av type ${varselHendelse.type}")
         when (varselHendelse.type) {
             NL_OPPFOLGINGSPLAN_OPPRETTET,
-            NL_OPPFOLGINGSPLAN_SENDT_TIL_GODKJENNING -> oppfolgingsplanVarselService.sendVarselTilDineSykmeldte(varselHendelse)
-            NL_DIALOGMOTE_SVAR_MOTEBEHOV -> motebehovVarselService.sendVarselTilNarmesteLeder(varselHendelse)
-            SM_DIALOGMOTE_SVAR_MOTEBEHOV -> motebehovVarselService.sendVarselTilSykmeldt(varselHendelse)
+            NL_OPPFOLGINGSPLAN_SENDT_TIL_GODKJENNING -> oppfolgingsplanVarselService.sendVarselTilNarmesteLeder(varselHendelse.toNarmestelederHendelse())
+            NL_DIALOGMOTE_SVAR_MOTEBEHOV -> motebehovVarselService.sendVarselTilNarmesteLeder(varselHendelse.toNarmestelederHendelse())
+            SM_DIALOGMOTE_SVAR_MOTEBEHOV -> motebehovVarselService.sendVarselTilArbeidstaker(varselHendelse.toArbeidstakerHendelse())
         }
+    }
+
+    private fun EsyfovarselHendelse.toNarmestelederHendelse(): NarmesteLederHendelse {
+        return if (this is NarmesteLederHendelse) {
+            this
+        } else throw IllegalArgumentException("Wrong type of EsyfovarselHendelse, should be of type NarmesteLederHendelse")
+    }
+
+    private fun EsyfovarselHendelse.toArbeidstakerHendelse(): ArbeidstakerHendelse {
+        return if (this is ArbeidstakerHendelse) {
+            this
+        } else throw IllegalArgumentException("Wrong type of EsyfovarselHendelse, should be of type ArbeidstakerHendelse")
     }
 }
