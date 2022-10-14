4ebb4f72393d57239b638dce11e3e05b7e14b0c1
Tanja Krasnomovets <tanja.krasnomovets@nav.no>
2022-10-14 10:37:27 +0200

    Make IS10_UTBET_TOM nullable

--- src/main/kotlin/no/nav/syfo/kafka/consumers/infotrygd/domain/KInfotrygdSykepengedager.kt
+++ src/main/kotlin/no/nav/syfo/kafka/consumers/infotrygd/domain/KInfotrygdSykepengedager.kt
@@ -1,18 +1,20 @@
 package no.nav.syfo.kafka.consumers.infotrygd.domain
 
 import com.fasterxml.jackson.annotation.JsonAlias
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class KInfotrygdSykepengedager(
     val after: After,
 ) {
     data class After(
         @JsonProperty("IS10_UTBET_TOM")
         @JsonAlias("is10_UTBET_TOM")
-        val IS10_UTBET_TOM: String,
+        val IS10_UTBET_TOM: String?,
         @JsonProperty("IS10_MAX")
         @JsonAlias("is10_MAX")
-        val IS10_MAX: String,
+        val IS10_MAX: String?,
         @JsonProperty("F_NR")
         @JsonAlias("f_NR")
         val F_NR: String,
