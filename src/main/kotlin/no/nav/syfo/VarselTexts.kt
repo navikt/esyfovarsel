4b378134f7b01bfbd3898b5bbf7bc0f2adac215a
Tanja Krasnomovets <tanja.krasnomovets@nav.no>
2022-10-28 09:55:13 +0200

    Update arbeidsgivernotifikasjon email text

--- src/main/kotlin/no/nav/syfo/VarselTexts.kt
+++ src/main/kotlin/no/nav/syfo/VarselTexts.kt
@@ -11,15 +11,17 @@ const val BRUKERNOTIFIKASJONER_MER_VEILEDNING_MESSAGE_TEXT = "Det nærmer seg da
 const val ARBEIDSGIVERNOTIFIKASJON_MERKELAPP = "Oppfølging"
 const val ARBEIDSGIVERNOTIFIKASJON_AKTIVITETSKRAV_MESSAGE_TEXT = "En av dine medarbeidere har snart vært sykmeldt i åtte uker"
 const val ARBEIDSGIVERNOTIFIKASJON_AKTIVITETSKRAV_EMAIL_TITLE = "Varsel om nytt aktivitetskrav"
-const val ARBEIDSGIVERNOTIFIKASJON_AKTIVITETSKRAV_EMAIL_BODY = "<body>En av dine medarbeidere har snart vært sykmeldt i åtte uker. Logg inn på Din side arbeidsgiver på nav.no for å se hvem det gjelder.</body>"
+const val ARBEIDSGIVERNOTIFIKASJON_AKTIVITETSKRAV_EMAIL_BODY =
+    "<body>En av dine medarbeidere har snart vært sykmeldt i åtte uker. Logg inn på Din side arbeidsgiver på nav.no for å se hvem det gjelder.</body>"
 
 const val ARBEIDSGIVERNOTIFIKASJON_SVAR_MOTEBEHOV_MESSAGE_TEXT = "Vi trenger din vurdering av behovet for dialogmøte."
 const val ARBEIDSGIVERNOTIFIKASJON_SVAR_MOTEBEHOV_EMAIL_TITLE = "Behov for dialogmøte?"
 const val ARBEIDSGIVERNOTIFIKASJON_SVAR_MOTEBEHOV_EMAIL_BODY = "<body>Hei.<br>\n" +
-    "NAV trenger din vurdering av behovet for dialogmøte, i forbindelse med en av dine ansatte sitt sykefravær.<br>\n" +
-    "Du kan svare på møtebehovet ved å logge deg inn på “Min side - arbeidsgiver” og trykke på “Sykmeldte”. Da vil du også se hvem det gjelder.<br><br>\n" +
-    "Har du spørsmål, kan du kontakte oss på 55 55 33 36.<br>\n" +
-    "Vennlig hilsen NAV<br>\n" +
-    "Du kan ikke svare på denne meldingen.</body>"
+        "NAV trenger ditt svar på om du har behov for dialogmøte i forbindelse med en av dine ansatte sitt sykefravær.<br>\n" +
+        "Du svarer på behovet ved å logge deg inn på \"Min side - arbeidsgiver\". I \"bjella\" ser du hvem oppgaven gjelder. <br>\n" +
+        "Dette er din mulighet til å påvirke om det skal avholdes et dialogmøte eller ikke. <br><br>\n" +
+        "Har du spørsmål, kan du kontakte oss på 55 55 33 36.<br>\n" +
+        "Vennlig hilsen NAV<br>\n" +
+        "Du kan ikke svare på denne meldingen.</body>"
 
 const val DITT_SYKEFRAVAER_MER_VEILEDNING_MESSAGE_TEXT = "Snart slutt på sykepengene"
