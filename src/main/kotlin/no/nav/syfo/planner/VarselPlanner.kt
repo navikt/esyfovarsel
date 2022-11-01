6e19acba7463c985eb413859e637ab8c88f4d202
Tanja Krasnomovets <tanja.krasnomovets@nav.no>
2022-11-01 12:34:43 +0100

    Delete redundant method

--- src/main/kotlin/no/nav/syfo/planner/VarselPlanner.kt
+++ src/main/kotlin/no/nav/syfo/planner/VarselPlanner.kt
@@ -1,11 +1,6 @@
 package no.nav.syfo.planner
 
-import no.nav.syfo.access.domain.UserAccessStatus
-
 interface VarselPlanner {
     val name: String
     suspend fun processSyketilfelle(fnr: String, orgnummer: String?)
-    fun varselSkalLagres(userAccessStatus: UserAccessStatus): Boolean {
-        return userAccessStatus.canUserBeDigitallyNotified
-    }
 }
