bc535db7b34756bc5eea77aca027b40568c0bc91
Kaja Kvinge Akselsen <75254007+kaja-nav@users.noreply.github.com>
2022-11-10 13:52:52 +0100

    Fix test and query (#237)

--- src/test/kotlin/no/nav/syfo/db/UtbetalingerDAOSpek.kt
+++ src/test/kotlin/no/nav/syfo/db/UtbetalingerDAOSpek.kt
@@ -4,19 +4,20 @@ import no.nav.syfo.db.domain.PUtbetaling
 import no.nav.syfo.kafka.consumers.utbetaling.domain.UtbetalingUtbetalt
 import no.nav.syfo.testutil.EmbeddedDatabase
 import no.nav.syfo.testutil.dropData
+import org.amshove.kluent.shouldHaveSingleItem
 import org.amshove.kluent.shouldMatchAtLeastOneOf
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 import java.time.LocalDate
 import java.time.LocalDate.now
-
+import java.util.*
 
 object UtbetalingerDAOSpek : Spek({
 
     //The default timeout of 10 seconds is not sufficient to initialise the embedded database
     defaultTimeout = 20000L
 
-    describe("SykepengerMaxDateDAOSpek") {
+    describe("UtbetalingerDAOSpek") {
         val embeddedDatabase by lazy { EmbeddedDatabase() }
 
         afterEachTest {
@@ -27,10 +28,13 @@ object UtbetalingerDAOSpek : Spek({
             embeddedDatabase.stop()
         }
 
-        xit("Should fetch utbetalinger that should result in mer veiledning varsel") {
+        it("Should fetch utbetalinger that should result in mer veiledning varsel") {
             val spleisUtbetaling1 = spleisUtbetaling(tom = now().minusDays(3), forelopigBeregnetSluttPaSykepenger = now().plusDays(15), gjenstaendeSykedager = 79)
             embeddedDatabase.storeSpleisUtbetaling(spleisUtbetaling1)
+            embeddedDatabase.storeInfotrygdUtbetaling(arbeidstakerFnr1, now().plusMonths(3), now().minusMonths(1), 60)
+
             val merVeiledningVarslerToSend = embeddedDatabase.fetchMerVeiledningVarslerToSend()
+            merVeiledningVarslerToSend.shouldHaveSingleItem()
             merVeiledningVarslerToSend.skalInneholde(
                 spleisUtbetaling1.tom,
                 spleisUtbetaling1.foreløpigBeregnetSluttPåSykepenger!!,
@@ -42,19 +46,19 @@ object UtbetalingerDAOSpek : Spek({
 })
 
 private fun spleisUtbetaling(tom: LocalDate, forelopigBeregnetSluttPaSykepenger: LocalDate, gjenstaendeSykedager: Int) = UtbetalingUtbetalt(
-    arbeidstakerFnr1,
-    "123",
-    "utbetalt",
-    null,
-    forelopigBeregnetSluttPaSykepenger,
-    100,
-    gjenstaendeSykedager,
-    100,
-    1,
-    forelopigBeregnetSluttPaSykepenger.minusDays(100),
-    tom,
-    "123",
-    "123"
+    fødselsnummer = arbeidstakerFnr1,
+    organisasjonsnummer = "234",
+    event = "ubetaling_utbetalt",
+    type = "UTBETALING",
+    foreløpigBeregnetSluttPåSykepenger = forelopigBeregnetSluttPaSykepenger,
+    forbrukteSykedager = 100,
+    gjenståendeSykedager = gjenstaendeSykedager,
+    stønadsdager = 10,
+    antallVedtak = 4,
+    fom = now().minusDays(50),
+    tom = tom,
+    utbetalingId = UUID.randomUUID().toString(),
+    korrelasjonsId = UUID.randomUUID().toString(),
 )
 
 
