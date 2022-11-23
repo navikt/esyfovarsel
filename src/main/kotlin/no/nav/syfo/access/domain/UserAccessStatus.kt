85d667139f83163b2bdacb3fd8a0939ba407325d
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2022-11-23 08:24:55 +0100

    Fix compilation error after revert

--- src/main/kotlin/no/nav/syfo/access/domain/UserAccessStatus.kt
+++ src/main/kotlin/no/nav/syfo/access/domain/UserAccessStatus.kt
@@ -5,3 +5,5 @@ data class UserAccessStatus(
     val canUserBeDigitallyNotified: Boolean,
     val canUserBePhysicallyNotified: Boolean,
 )
+
+fun UserAccessStatus.canUserBeNotified(): Boolean = canUserBeDigitallyNotified || canUserBePhysicallyNotified
