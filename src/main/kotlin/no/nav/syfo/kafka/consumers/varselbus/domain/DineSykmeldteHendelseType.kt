b8f2665b532189f0d636d23fd7cd84df492c1f0a
Tanja Krasnomovets <tanja.krasnomovets@nav.no>
2022-12-05 10:23:57 +0100

    QA: Move toDineSykmeldteHendelseType from esyfovarsel

--- src/main/kotlin/no/nav/syfo/kafka/consumers/varselbus/domain/DineSykmeldteHendelseType.kt
+++ src/main/kotlin/no/nav/syfo/kafka/consumers/varselbus/domain/DineSykmeldteHendelseType.kt
@@ -10,3 +10,18 @@ enum class DineSykmeldteHendelseType {
     DIALOGMOTE_ENDRING,
     DIALOGMOTE_REFERAT,
 }
+
+fun HendelseType.toDineSykmeldteHendelseType(): DineSykmeldteHendelseType {
+    return when (this) {
+        HendelseType.NL_OPPFOLGINGSPLAN_SENDT_TIL_GODKJENNING -> DineSykmeldteHendelseType.OPPFOLGINGSPLAN_TIL_GODKJENNING
+        HendelseType.NL_OPPFOLGINGSPLAN_OPPRETTET -> DineSykmeldteHendelseType.OPPFOLGINGSPLAN_OPPRETTET
+        HendelseType.NL_DIALOGMOTE_SVAR_MOTEBEHOV -> DineSykmeldteHendelseType.DIALOGMOTE_SVAR_BEHOV
+        HendelseType.NL_DIALOGMOTE_INNKALT -> DineSykmeldteHendelseType.DIALOGMOTE_INNKALLING
+        HendelseType.NL_DIALOGMOTE_AVLYST -> DineSykmeldteHendelseType.DIALOGMOTE_AVLYSNING
+        HendelseType.NL_DIALOGMOTE_NYTT_TID_STED -> DineSykmeldteHendelseType.DIALOGMOTE_ENDRING
+        HendelseType.NL_DIALOGMOTE_REFERAT -> DineSykmeldteHendelseType.DIALOGMOTE_REFERAT
+        else -> {
+            throw IllegalArgumentException("Kan ikke mappe ${this.name} til en DineSykmeldteHendelsesType")
+        }
+    }
+}
