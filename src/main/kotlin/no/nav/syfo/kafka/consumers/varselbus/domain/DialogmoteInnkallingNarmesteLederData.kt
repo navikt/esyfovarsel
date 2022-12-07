ee7e33703fb52d5b60518a8561a29f9a695da797
Vitenok <victorandreevi4@gmail.com>
2022-12-07 13:28:58 +0100

    Annotation

--- src/main/kotlin/no/nav/syfo/kafka/consumers/varselbus/domain/DialogmoteInnkallingNarmesteLederData.kt
+++ src/main/kotlin/no/nav/syfo/kafka/consumers/varselbus/domain/DialogmoteInnkallingNarmesteLederData.kt
@@ -1,7 +1,9 @@
 package no.nav.syfo.kafka.consumers.varselbus.domain
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo
 import java.io.Serializable
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME)
 data class DialogmoteInnkallingNarmesteLederData(
     val narmesteLederNavn: String?,
 ) : Serializable
