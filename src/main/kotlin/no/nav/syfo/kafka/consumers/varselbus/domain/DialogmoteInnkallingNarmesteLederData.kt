c6ca604bcf4ccdea954646d356a71b210daf8e83
Vitenok <victorandreevi4@gmail.com>
2022-12-08 10:34:49 +0100

    roperty = "type"

--- src/main/kotlin/no/nav/syfo/kafka/consumers/varselbus/domain/DialogmoteInnkallingNarmesteLederData.kt
+++ src/main/kotlin/no/nav/syfo/kafka/consumers/varselbus/domain/DialogmoteInnkallingNarmesteLederData.kt
@@ -3,7 +3,7 @@ package no.nav.syfo.kafka.consumers.varselbus.domain
 import com.fasterxml.jackson.annotation.JsonTypeInfo
 import java.io.Serializable
 
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "@type")
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
 data class DialogmoteInnkallingNarmesteLederData(
     val narmesteLederNavn: String?,
 ) : Serializable
