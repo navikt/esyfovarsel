362017bc8f268947b3ae0b5829816f9da7772e22
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2023-05-23 11:43:59 +0200

    Move ferdigstill field to EsyfovarselHendelse - part 1
    To make it backwards compatible the field will first be added to EsyfovarselHendelse, and when the client is updated to use the new field, the old field will later be removed from VarselData.

--- src/main/kotlin/no/nav/syfo/service/SendVarselService.kt
+++ src/main/kotlin/no/nav/syfo/service/SendVarselService.kt
@@ -149,6 +149,7 @@ class SendVarselService(
         merVeiledningVarselService.sendVarselTilArbeidstaker(
             ArbeidstakerHendelse(
                 HendelseType.SM_MER_VEILEDNING,
+                false,
                 null,
                 pPlanlagtVarsel.fnr,
                 null
