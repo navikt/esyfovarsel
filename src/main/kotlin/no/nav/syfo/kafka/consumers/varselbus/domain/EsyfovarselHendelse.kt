d8b4834d1bc4a7b711f31e0ec5108a005af4ded8
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2023-05-23 08:51:49 +0200

    Move ferdigstill field to EsyfovarselHendelse - part 1
    To make it backwards compatible the field will first be added to EsyfovarselHendelse, and when the client is updated to use the new field, the old field will later be removed from VarselData.

--- src/main/kotlin/no/nav/syfo/kafka/consumers/varselbus/domain/EsyfovarselHendelse.kt
+++ src/main/kotlin/no/nav/syfo/kafka/consumers/varselbus/domain/EsyfovarselHendelse.kt
@@ -9,11 +9,13 @@ private val objectMapper = createObjectMapper()
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME)
 sealed interface EsyfovarselHendelse : Serializable {
     val type: HendelseType
+    val ferdigstill: Boolean?
     var data: Any?
 }
 
 data class NarmesteLederHendelse(
     override val type: HendelseType,
+    override val ferdigstill: Boolean?,
     override var data: Any?,
     val narmesteLederFnr: String,
     val arbeidstakerFnr: String,
@@ -22,6 +24,7 @@ data class NarmesteLederHendelse(
 
 data class ArbeidstakerHendelse(
     override val type: HendelseType,
+    override val ferdigstill: Boolean?,
     override var data: Any?,
     val arbeidstakerFnr: String,
     val orgnummer: String?
