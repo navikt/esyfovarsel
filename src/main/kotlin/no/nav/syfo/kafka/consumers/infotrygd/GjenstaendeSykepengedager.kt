09b45bd77e8b2bd4ad9449a709bf5b110c642a1e
Kaja Kvinge Akselsen <75254007+kaja-nav@users.noreply.github.com>
2023-05-31 12:15:31 +0200

    Handle when maxDate is before utebetTom  date (#318)

--- src/main/kotlin/no/nav/syfo/kafka/consumers/infotrygd/GjenstaendeSykepengedager.kt
+++ src/main/kotlin/no/nav/syfo/kafka/consumers/infotrygd/GjenstaendeSykepengedager.kt
@@ -1,16 +1,18 @@
 package no.nav.syfo.kafka.consumers.infotrygd
 
+import no.nav.syfo.utils.isEqualOrBefore
 import java.time.DayOfWeek
 import java.time.LocalDate
 import kotlin.streams.toList
 
 fun LocalDate.gjenstaendeSykepengedager(other: LocalDate): Int {
-    return this
+    // datesUntil teller med startdato og ikke sluttdato, mens vi ikke vil telle med startdato, men sluttdato, derfor er det lagt til en dag på begge datoene
+    return if (this.isEqualOrBefore(other)) this
         .plusDays(1)
         .datesUntil(other.plusDays(1))
         .toList()
         .count(LocalDate::erIkkeHelg)
-        .coerceAtLeast(0)
+        .coerceAtLeast(0) else 0
 }
 
 private fun LocalDate.erIkkeHelg() = dayOfWeek !in arrayOf(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY)
