a42ccc7c2731a09fcef9781de5da3ac06f1aeeff
Tor-Christian Tangenes <40393162+torchr89@users.noreply.github.com>
2023-06-09 14:06:09 +0200

    Use 'behandlingsnummer'-header in request to PDL instead of 'tema'-he… (#322)
    * Use 'behandlingsnummer'-header in request to PDL instead of 'tema'-header
    
    * Minor linting

--- src/main/kotlin/no/nav/syfo/consumer/pdl/Constants.kt
+++ src/main/kotlin/no/nav/syfo/consumer/pdl/Constants.kt
@@ -1,7 +1,9 @@
 package no.nav.syfo.consumer.pdl
 
-const val TEMA_HEADER = "Tema"
-const val OPPFOLGING_TEMA_HEADERVERDI = "OPP"
+// Lenke til relevant behandling i behandlingskatalogen:
+// https://behandlingskatalog.nais.adeo.no/process/team/6a3b85e0-0e06-4f58-95bb-4318e31c4b2b/60868e2d-b151-47a3-a1a6-1d896fc3a88d
+const val BEHANDLINGSNUMMER_VURDERE_RETT_TIL_SYKEPENGER = "B426"
+const val PDL_BEHANDLINGSNUMMER_HEADER = "behandlingsnummer"
 
 const val QUERY_PATH_PREFIX = "/pdl"
 const val IDENTER_QUERY = "hentIdenter.graphql"
