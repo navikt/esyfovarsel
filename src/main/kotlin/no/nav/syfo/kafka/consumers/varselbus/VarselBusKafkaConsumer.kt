22237c78d2687bbd5d4b5809f9adfab2e00b6bb7
Tor-Christian Tangenes <torchrct@gmail.com>
2023-08-15 19:12:24 +0200

    Stop varselbus record processing by shutdown of application on processing error

--- src/main/kotlin/no/nav/syfo/kafka/consumers/varselbus/VarselBusKafkaConsumer.kt
+++ src/main/kotlin/no/nav/syfo/kafka/consumers/varselbus/VarselBusKafkaConsumer.kt
@@ -7,6 +7,8 @@ import no.nav.syfo.Environment
 import no.nav.syfo.kafka.common.*
 import no.nav.syfo.kafka.consumers.varselbus.domain.EsyfovarselHendelse
 import no.nav.syfo.service.VarselBusService
+import no.nav.syfo.shutdownApplication
+import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -15,7 +17,7 @@ class VarselBusKafkaConsumer(
     val env: Environment,
     val varselBusService: VarselBusService
 ) : KafkaListener {
-    private val log: Logger = LoggerFactory.getLogger("no.nav.syfo.kafka.VarselBusConsumer")
+    private val log: Logger = LoggerFactory.getLogger(VarselBusKafkaConsumer::class.qualifiedName)
     private val kafkaListener: KafkaConsumer<String, String>
     private val objectMapper = createObjectMapper()
 
@@ -28,26 +30,24 @@ class VarselBusKafkaConsumer(
     override suspend fun listen(applicationState: ApplicationState) {
         log.info("Started listening to topic $topicVarselBus")
         while (applicationState.running) {
-            kafkaListener.poll(pollDurationInMillis).forEach {
-                try {
-                    val varselEvent: EsyfovarselHendelse = objectMapper.readValue(it.value())
-                    varselEvent.data = objectMapper.readTree(it.value())["data"]
-                    log.info("VARSEL BUS: Mottatt melding med UUID ${it.key()} av type: ${varselEvent.type}")
-                    varselBusService.processVarselHendelse(varselEvent)
-                    varselBusService.processVarselHendelseAsMinSideMicrofrontendEvent(varselEvent)
-                } catch (e: IOException) {
-                    log.error(
-                        "Error in [$topicVarselBus]-listener: Could not parse message | ${e.message}",
-                        e
-                    )
-                } catch (e: Exception) {
-                    log.error(
-                        "Exception in [$topicVarselBus]-listener: ${e.message}",
-                        e
-                    )
-                }
+            try {
+                kafkaListener.poll(pollDurationInMillis).forEach { processVarselBusRecord(it) }
                 kafkaListener.commitSync()
+            } catch (e: Exception) {
+                log.error(
+                    "Exception in [$topicVarselBus]-listener: ${e.message}",
+                    e
+                )
+                applicationState.shutdownApplication()
             }
         }
+    }
+
+    private fun processVarselBusRecord(record: ConsumerRecord<String, String>) {
+        val varselEvent: EsyfovarselHendelse = objectMapper.readValue(record.value())
+        varselEvent.data = objectMapper.readTree(record.value())["data"]
+        log.info("VARSEL BUS: Mottatt melding med UUID ${record.key()} av type: ${varselEvent.type}")
+        varselBusService.processVarselHendelse(varselEvent)
+        varselBusService.processVarselHendelseAsMinSideMicrofrontendEvent(varselEvent)
     }
 }
