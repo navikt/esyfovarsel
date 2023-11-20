0a7dc43494a026a44dd4720bd0ab0f9d6013d9dd
Tor-Christian Tangenes <torchrct@gmail.com>
2023-11-20 11:52:16 +0100

    Typo

--- src/main/kotlin/no/nav/syfo/kafka/consumers/varselbus/domain/EsyfovarselHendelse.kt
+++ src/main/kotlin/no/nav/syfo/kafka/consumers/varselbus/domain/EsyfovarselHendelse.kt
@@ -152,11 +152,11 @@ fun HendelseType.isDialogmoteType() = this.isDialogmoteInnkallingType() or (
 fun ArbeidstakerHendelse.notCorrectMikrofrontendType() =
     !(this.type.isDialogmoteType() or this.type.isAktivitetspliktType())
 
-fun ArbeidstakerHendelse.isAktivtetspliktWithFerdigstilling() =
+fun ArbeidstakerHendelse.isAktivitetspliktWithFerdigstilling() =
     (this.type.isAktivitetspliktType() and (this.ferdigstill == true))
 
 fun ArbeidstakerHendelse.isNotEligibleForMikrofrontendProcessing(): Boolean {
-    return this.notCorrectMikrofrontendType() or isAktivtetspliktWithFerdigstilling()
+    return this.notCorrectMikrofrontendType() or isAktivitetspliktWithFerdigstilling()
 }
 
 fun HendelseType.isNotValidHendelseType() = !this.isAktivitetspliktType() && !this.isDialogmoteInnkallingType()
