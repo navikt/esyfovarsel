11f1542d95537f9028309153dd79a7d9002d079a
Kaja Kvinge Akselsen <75254007+kaja-nav@users.noreply.github.com>
2024-04-30 13:17:42 +0200

    Revert "Get correct pdf for reserved and digital users (#493)" (#495)
    This reverts commit b05d2e56dfcdab43cb7177a7f409ff5b2a8765f5.

--- src/main/kotlin/no/nav/syfo/service/MerVeiledningVarselService.kt
+++ src/main/kotlin/no/nav/syfo/service/MerVeiledningVarselService.kt
@@ -35,7 +35,7 @@ class MerVeiledningVarselService(
         val isBrukerReservert = !userAccessStatus.canUserBeDigitallyNotified
 
         if (isBrukerReservert) {
-            val pdf = pdfgenConsumer.getMerVeiledningPdfForReserverte(arbeidstakerHendelse.arbeidstakerFnr)
+            val pdf = pdfgenConsumer.getMerVeiledningPDF(arbeidstakerHendelse.arbeidstakerFnr, isBrukerReservert = true)
 
             val journalpostId = pdf?.let {
                 dokarkivService.getJournalpostId(
@@ -50,7 +50,7 @@ class MerVeiledningVarselService(
             sendBrevVarselTilArbeidstaker(planlagtVarselUuid, arbeidstakerHendelse, journalpostId!!)
         } else {
             val pdf =
-                pdfgenConsumer.getMerVeiledningPdfForDigitale(arbeidstakerHendelse.arbeidstakerFnr)
+                pdfgenConsumer.getMerVeiledningPDF(arbeidstakerHendelse.arbeidstakerFnr, isBrukerReservert = false)
 
             val journalpostId = pdf?.let {
                 dokarkivService.getJournalpostId(
