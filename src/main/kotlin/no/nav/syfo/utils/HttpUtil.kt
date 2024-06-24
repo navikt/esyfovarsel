b2abb7faa795bfcdcdaa4326eea61caa10ad22e1
AudunSorheim <audun.sorheim@nav.no>
2024-06-24 12:51:12 +0200

    Increase timeout

--- src/main/kotlin/no/nav/syfo/utils/HttpUtil.kt
+++ src/main/kotlin/no/nav/syfo/utils/HttpUtil.kt
@@ -5,8 +5,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
-import io.ktor.client.plugins.HttpRequestRetry
-import io.ktor.client.plugins.ClientRequestException
+import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.jackson.jackson
 
@@ -36,6 +35,9 @@ fun httpClientWithRetry(): HttpClient {
                 registerModule(JavaTimeModule())
                 configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
             }
+        }
+        install(HttpTimeout) {
+            requestTimeoutMillis = 60000
         }
     }
 }
