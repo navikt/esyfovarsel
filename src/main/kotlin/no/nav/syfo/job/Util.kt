6bd987856666e6fcfe296fab471378acd1f03de9
AudunSorheim <80095835+AudunSorheim@users.noreply.github.com>
2024-10-30 14:08:46 +0100

    Delete meroppfolging job (#591)
    * Delete meroppfolging job

--- src/main/kotlin/no/nav/syfo/job/Util.kt
+++ src/main/kotlin/no/nav/syfo/job/Util.kt
@@ -9,28 +9,24 @@ import no.nav.syfo.utils.httpClient
 import org.slf4j.LoggerFactory
 import java.util.*
 
-fun sendNotificationsJob(env: JobEnv) {
+fun closeExpiredMicrofrontendsJob(env: JobEnv) {
     val logg = LoggerFactory.getLogger("no.nav.syfo.job.Util")
-    if (env.sendVarsler) {
-        runBlocking {
-            logg.info("Starter jobb")
-            val credentials = "${env.serviceuserUsername}:${env.serviceuserPassword}"
-            val encodededCredentials = Base64.getEncoder().encodeToString(credentials.toByteArray())
-            val httpClient = httpClient()
-            val response: HttpResponse = httpClient.post(env.jobTriggerUrl) {
-                headers {
-                    append("Authorization", "Basic $encodededCredentials")
-                }
+    runBlocking {
+        logg.info("Starter jobb")
+        val credentials = "${env.serviceuserUsername}:${env.serviceuserPassword}"
+        val encodededCredentials = Base64.getEncoder().encodeToString(credentials.toByteArray())
+        val httpClient = httpClient()
+        val response: HttpResponse = httpClient.post(env.jobTriggerUrl) {
+            headers {
+                append("Authorization", "Basic $encodededCredentials")
             }
-            val status = response.status
-            if (status == HttpStatusCode.OK) {
-                logg.info("Har trigget varselutsending")
-            } else {
-                logg.error("Feil i esyfovarsel-job: Klarte ikke kalle trigger-API i esyfovarsel. Fikk svar med status: $status")
-            }
-            httpClient.close()
         }
-    } else {
-        logg.info("Jobb togglet av")
+        val status = response.status
+        if (status == HttpStatusCode.OK) {
+            logg.info("Jobb startet")
+        } else {
+            logg.error("Feil i esyfovarsel-job: Klarte ikke kalle trigger-API i esyfovarsel. Fikk svar med status: $status")
+        }
+        httpClient.close()
     }
 }
