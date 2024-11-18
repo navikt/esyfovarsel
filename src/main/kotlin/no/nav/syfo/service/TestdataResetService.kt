34b5315695ef8190d340175abacd29bc7947e868
Tanja Krasnomovets <Vitenok@users.noreply.github.com>
2024-11-18 11:54:56 +0100

    Revert "Revert "Delete maxdate api (#602)" (#603)"
    This reverts commit ed26f06fdda57836e0bbd0830a13f282a22d15e6.

--- src/main/kotlin/no/nav/syfo/service/TestdataResetService.kt
+++ src/main/kotlin/no/nav/syfo/service/TestdataResetService.kt
@@ -2,12 +2,8 @@ package no.nav.syfo.service
 
 import no.nav.syfo.db.DatabaseInterface
 import no.nav.syfo.db.deleteMikrofrontendSynlighetByFnr
-import no.nav.syfo.db.deletePlanlagtVarselByVarselId
-import no.nav.syfo.db.deleteUtbetalingInfotrygdByFnr
-import no.nav.syfo.db.deleteUtbetalingSpleisByFnr
 import no.nav.syfo.db.deleteUtsendtVarselByFnr
 import no.nav.syfo.db.deleteUtsendtVarselFeiletByFnr
-import no.nav.syfo.db.fetchPlanlagtVarselByFnr
 import no.nav.syfo.domain.PersonIdent
 import no.nav.syfo.service.microfrontend.MikrofrontendService
 import org.slf4j.Logger
@@ -25,7 +21,7 @@ class TestdataResetService(
             "Nullstiller testdata for arbeidstaker ${fnr.value}. Ferdigstiller varsler " +
                     "som er sendt til Min side - arbeidsgiver, Dine sykmeldte, Min side (personbruker), " +
                     "Ditt sykefravær. Fjerner mikrofrontend på Min side. " +
-                    "Sletter planlagte og utsendte varsler, feilede varsler og utbetalinger."
+                    "Sletter planlagte og utsendte varsler, feilede varsler."
         )
 
         // Ferdigstille varsler
@@ -34,21 +30,11 @@ class TestdataResetService(
         // Slette utsendte varsler
         database.deleteUtsendtVarselByFnr(fnr)
 
-        // Slette planlagte varsler
-        val planlagteVarsler = database.fetchPlanlagtVarselByFnr(fnr.value)
-        planlagteVarsler.forEach { database.deletePlanlagtVarselByVarselId(it.uuid) }
-
         // Slette/skjule mikrofrontend
         mikrofrontendService.closeAllMikrofrontendForUser(fnr)
         database.deleteMikrofrontendSynlighetByFnr(fnr)
 
         // Slette utsendt varsel feilet
         database.deleteUtsendtVarselFeiletByFnr(fnr)
-
-        // Slette utbetalinger fra Spleis
-        database.deleteUtbetalingSpleisByFnr(fnr)
-
-        // Slette utbetalinger fra infotrygd
-        database.deleteUtbetalingInfotrygdByFnr(fnr)
     }
 }
