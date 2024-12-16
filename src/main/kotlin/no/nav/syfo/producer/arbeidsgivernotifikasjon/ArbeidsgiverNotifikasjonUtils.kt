63054d4df1745249190273715d372cfe4ce93107
AudunSorheim <audun.sorheim@nav.no>
2024-12-16 15:05:20 +0100

    wip

--- src/main/kotlin/no/nav/syfo/producer/arbeidsgivernotifikasjon/ArbeidsgiverNotifikasjonUtils.kt
+++ src/main/kotlin/no/nav/syfo/producer/arbeidsgivernotifikasjon/ArbeidsgiverNotifikasjonUtils.kt
@@ -3,4 +3,7 @@ package no.nav.syfo.producer.arbeidsgivernotifikasjon
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-fun LocalDateTime.formatAsISO8601(): String = this.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+fun LocalDateTime.formatAsISO8601(): String {
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
+    return this.format(formatter)
+}
