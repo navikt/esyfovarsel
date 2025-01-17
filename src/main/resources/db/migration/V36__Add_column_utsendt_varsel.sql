f1bf134336bbf979729175811fba2d85f6d3ab6c
Tanja Krasnomovets <victorandreevi4@gmail.com>
2025-01-17 09:54:28 +0100

    Add if not exists

--- src/main/resources/db/migration/V36__Add_column_utsendt_varsel.sql
+++ src/main/resources/db/migration/V36__Add_column_utsendt_varsel.sql
@@ -1,5 +1,5 @@
 ALTER TABLE UTSENDT_VARSEL
-    ADD COLUMN is_forced_letter boolean;
+    ADD COLUMN if not exists is_forced_letter boolean;
 
 ALTER TABLE UTSENDING_VARSEL_FEILET
-    ADD COLUMN is_forced_letter boolean;
+    ADD COLUMN if not exists is_forced_letter boolean;
