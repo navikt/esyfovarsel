12ee5d2549510f902fa0999aaba24a7bfb2666e4
Audun Sørheim <audun.sorheim@nav.no>
2025-03-05 14:15:58 +0100

    Change param in JournalpostdistribusjonRequest

--- src/main/kotlin/no/nav/syfo/consumer/distribuerjournalpost/JournalpostdistribusjonRequest.kt
+++ src/main/kotlin/no/nav/syfo/consumer/distribuerjournalpost/JournalpostdistribusjonRequest.kt
@@ -1,10 +1,15 @@
 package no.nav.syfo.consumer.distribuerjournalpost
 
+enum class Kanal {
+    PRINT,
+    TRYGDERETTEN
+}
+
 data class JournalpostdistribusjonRequest(
     val journalpostId: String,
     val bestillendeFagsystem: String = "UKJENT",
     val dokumentProdApp: String = "esyfovarsel",
     val distribusjonstype: String,
     val distribusjonstidspunkt: String = "UMIDDELBART",
-    val tvingSentralPrint: Boolean? = false,
+    val tvingKanal: Kanal? = null,
 )
