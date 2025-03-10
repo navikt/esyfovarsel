67e8b41bc786ba56b90c90e9c515c26ccc008bf9
Audun Sørheim <audun.sorheim@nav.no>
2025-03-10 15:36:35 +0100

    Send old dialogmote updates as beskjed

--- src/main/kotlin/no/nav/syfo/service/DialogmoteInnkallingNarmestelederVarselServiceOLD.kt
+++ src/main/kotlin/no/nav/syfo/service/DialogmoteInnkallingNarmestelederVarselServiceOLD.kt
@@ -63,7 +63,11 @@ class DialogmoteInnkallingNarmestelederVarselServiceOLD(
                 epostTittel = emailTitle,
                 epostHtmlBody = emailBody,
                 hardDeleteDate = LocalDateTime.now().plusWeeks(WEEKS_BEFORE_DELETE),
-                meldingstype = Meldingstype.BESKJED,
+                meldingstype = if (varselHendelse.type == NL_DIALOGMOTE_NYTT_TID_STED) {
+                    Meldingstype.OPPGAVE
+                } else {
+                    Meldingstype.BESKJED
+                },
                 grupperingsid = UUID.randomUUID().toString(),
             )
 
