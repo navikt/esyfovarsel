0132780b0e6f9d28c14b7d1844e102bd84d1b211
AudunSorheim <80095835+AudunSorheim@users.noreply.github.com>
2025-04-28 10:35:23 +0200

    Add try/catch to PdlClient (#710)

--- src/main/kotlin/no/nav/syfo/consumer/pdl/PdlClient.kt
+++ src/main/kotlin/no/nav/syfo/consumer/pdl/PdlClient.kt
@@ -1,6 +1,7 @@
 package no.nav.syfo.consumer.pdl
 
 import io.ktor.client.call.body
+import io.ktor.client.plugins.HttpRequestTimeoutException
 import io.ktor.client.request.header
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
@@ -26,33 +27,43 @@ open class PdlClient(private val urlEnv: UrlEnv, private val azureAdTokenConsume
         val query = getPdlQuery("/pdl/hentPerson.graphql")
         val request = PdlRequest(query, Variables(personIdent))
 
-        val response: HttpResponse = httpClient.post(urlEnv.pdlUrl) {
-            setBody(request)
-            header(HttpHeaders.ContentType, "application/json")
-            header(HttpHeaders.Authorization, "Bearer $token")
-            header(PDL_BEHANDLINGSNUMMER_HEADER, BEHANDLINGSNUMMER_VURDERE_RETT_TIL_SYKEPENGER)
-        }
+        try {
+            val response: HttpResponse = httpClient.post(urlEnv.pdlUrl) {
+                setBody(request)
+                header(HttpHeaders.ContentType, "application/json")
+                header(HttpHeaders.Authorization, "Bearer $token")
+                header(PDL_BEHANDLINGSNUMMER_HEADER, BEHANDLINGSNUMMER_VURDERE_RETT_TIL_SYKEPENGER)
+            }
 
-        when (response.status) {
-            HttpStatusCode.OK -> {
-                val pdlPersonReponse = response.body<HentPersonResponse>()
-                return if (!pdlPersonReponse.errors.isNullOrEmpty()) {
-                    COUNT_CALL_PDL_FAIL.increment()
-                    pdlPersonReponse.errors.forEach {
-                        log.error("Error while requesting person from PersonDataLosningen: ${it.errorMessage()}")
+            when (response.status) {
+                HttpStatusCode.OK -> {
+                    val pdlPersonReponse = response.body<HentPersonResponse>()
+                    return if (!pdlPersonReponse.errors.isNullOrEmpty()) {
+                        COUNT_CALL_PDL_FAIL.increment()
+                        pdlPersonReponse.errors.forEach {
+                            log.error("Error while requesting person from PersonDataLosningen: ${it.errorMessage()}")
+                        }
+                        null
+                    } else {
+                        COUNT_CALL_PDL_SUCCESS.increment()
+                        pdlPersonReponse.data
                     }
-                    null
-                } else {
-                    COUNT_CALL_PDL_SUCCESS.increment()
-                    pdlPersonReponse.data
+                }
+
+                else -> {
+                    COUNT_CALL_PDL_FAIL.increment()
+                    log.error("Request with url: $urlEnv.pdlUrl failed with response code ${response.status.value}")
+                    return null
                 }
             }
-
-            else -> {
-                COUNT_CALL_PDL_FAIL.increment()
-                log.error("Request with url: $urlEnv.pdlUrl failed with reponse code ${response.status.value}")
-                return null
-            }
+        } catch (e: HttpRequestTimeoutException) {
+            COUNT_CALL_PDL_FAIL.increment()
+            log.error("PDL request timed out: ${e.message}")
+            return null
+        } catch (e: Exception) {
+            COUNT_CALL_PDL_FAIL.increment()
+            log.error("Error calling PDL: ${e.message}")
+            return null
         }
     }
 
