72638b71c566015cec99b27715f61b0d374d2ab4
Solgård <vetle.bu.solgard@nav.no>
2025-08-26 15:21:55 +0200

    IS-3404: Legg til stotte for sending av kartleggingssporsmal

--- src/main/kotlin/no/nav/syfo/metrics/AppMetrics.kt
+++ src/main/kotlin/no/nav/syfo/metrics/AppMetrics.kt
@@ -1,6 +1,5 @@
 package no.nav.syfo.metrics
 
-import io.ktor.server.application.call
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.Routing
 import io.ktor.server.routing.get
@@ -15,6 +14,7 @@ const val METRICS_NS = "esyfovarsel"
 
 const val MER_VEILEDNING_NOTICE_SENT = "${METRICS_NS}_mer_veiledning_notice_sent"
 const val SVAR_MOTEBEHOV_NOTICE_SENT = "${METRICS_NS}_svar_motebehov_notice_sent"
+const val KARTLEGGINGSSPORSMAL_NOTICE_SENT = "${METRICS_NS}_kartleggingssporsmal_notice_sent"
 const val NOTICE_SENT = "${METRICS_NS}_notice_sent"
 const val CALL_PDL_SUCCESS = "${METRICS_NS}_call_pdl_success_count"
 const val CALL_PDL_FAIL = "${METRICS_NS}_call_pdl_fail_count"
@@ -25,6 +25,11 @@ val METRICS_REGISTRY =
 val COUNT_MER_VEILEDNING_NOTICE_SENT: Counter = Counter
     .builder(MER_VEILEDNING_NOTICE_SENT)
     .description("Counts the number of Mer veiledning notice sent")
+    .register(METRICS_REGISTRY)
+
+val COUNT_KARTLEGGINGSSPORSMAL_NOTICE_SENT: Counter = Counter
+    .builder(KARTLEGGINGSSPORSMAL_NOTICE_SENT)
+    .description("Counts the number of kartleggingssporsmal notice sent")
     .register(METRICS_REGISTRY)
 
 val COUNT_SVAR_MOTEBEHOV_NOTICE_SENT: Counter = Counter
@@ -48,6 +53,11 @@ val COUNT_CALL_PDL_FAIL: Counter = Counter.builder(CALL_PDL_FAIL)
 fun tellMerVeiledningVarselSendt() {
     COUNT_ALL_NOTICE_SENT.increment()
     COUNT_MER_VEILEDNING_NOTICE_SENT.increment()
+}
+
+fun countKartleggingssporsmalVarselSendt() {
+    COUNT_ALL_NOTICE_SENT.increment()
+    COUNT_KARTLEGGINGSSPORSMAL_NOTICE_SENT.increment()
 }
 
 fun tellSvarMotebehovVarselSendt(varslerSendt: Int) {
