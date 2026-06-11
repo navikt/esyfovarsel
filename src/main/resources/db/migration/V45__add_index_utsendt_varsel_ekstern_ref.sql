267fa7964c11f011ca4ddfff4f3ba30c10134323
danielengebretsen <daniel.engebretsen@nav.no>
2026-06-11 10:01:48 +0200

    remove CONCURRENTLY

--- src/main/resources/db/migration/V45__add_index_utsendt_varsel_ekstern_ref.sql
+++ src/main/resources/db/migration/V45__add_index_utsendt_varsel_ekstern_ref.sql
@@ -1,2 +1,1 @@
--- flyway:executeInTransaction=false
-CREATE INDEX CONCURRENTLY IF NOT EXISTS utsendt_varsel_ekstern_ref_index ON UTSENDT_VARSEL (ekstern_ref);
+CREATE INDEX IF NOT EXISTS utsendt_varsel_ekstern_ref_index ON UTSENDT_VARSEL (ekstern_ref);
